1540f87de4c3ee9d6b1880fb88c41c7379459e47
Mateusz Wit <mateusz.wit@travelport.com>
2019-03-06 09:32:58 +0100

    Prepare a basic layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const HelloWorld = styled.div`
-  font-size: 20px;
-  font-family: sans-serif;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  @media(min-width: 768px) {
+    flex-direction: row;
+    padding: 40px;
+  }
 `;
 
-const App = () => (
-  <HelloWorld>
-    HELLO WORLD
-  </HelloWorld>
-);
+const AddedCompanies = styled.div`
+  border: 2px solid black;
+  flex-grow: 4;
+  padding: 8px;
+`;
+
+const FoundCompanies = styled.div`
+  border-top: 2px solid black;
+  margin-top: 8px;
+  padding: 8px;
+`;
+
+const AddedCompaniesHeader = styled.div`
+  background: black;
+  color: white;
+  font-family: sans-serif;
+  font-size: 30px;
+  margin-bottom: 6px;
+  text-align: center;
+`;
+
+const SearchInput = styled.input`
+  border: 2px solid black;
+  margin-bottom: 2px;
+`;
+
+const FoundCompany = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2px;
+  margin-top: 2px;
+`;
+
+const SearchCompanyHeader = styled.div`
+  background: black;
+  color: white;
+  font-family: sans-serif;
+  font-size: 30px;
+  margin-bottom: 6px;
+  text-align: center;
+`;
+
+const SearchButton = styled.button`
+  border: 2px solid black;
+`;
+
+const Divider = styled.div`
+  flex-grow: 1;
+`;
+
+const Button = styled.button`
+  border: 2px solid black;
+`;
+
+const CompanyAdder = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 2;
+  padding: 8px;
+
+  @media(min-width: 768px) {
+    border: 2px solid black;
+  }
+`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      companies: [
+        { name: 'XYZ', price: 123 },
+      ],
+    };
+  }
+
+  render() {
+    const { state } = this;
+
+    return (
+      <Container>
+        <CompanyAdder>
+          <SearchCompanyHeader>
+            SEARCH
+          </SearchCompanyHeader>
+          <SearchInput />
+          <SearchButton>search</SearchButton>
+          <FoundCompanies>
+            <FoundCompany>
+              some company1
+              <Button>add</Button>
+            </FoundCompany>
+            <FoundCompany>
+              some company2
+              <Button>add</Button>
+            </FoundCompany>
+          </FoundCompanies>
+        </CompanyAdder>
+        <Divider />
+        <AddedCompanies>
+          <AddedCompaniesHeader>
+            ADDED COMPANIES
+          </AddedCompaniesHeader>
+          {JSON.stringify(state.companies)}
+        </AddedCompanies>
+      </Container>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
