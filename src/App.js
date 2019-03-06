2684ba3e4e6703f67f07d13cf685b1158566cc25
Mateusz Wit <mateusz.wit@travelport.com>
2019-03-06 13:21:08 +0100

    Search for companies using a symbol

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
+const nameAttr = '2. name';
+const symbolAttr = '1. symbol';
+
 const blackColor = '#000';
 const whiteColor = '#FFF';
 const grayColor = '#D3D3D3';
@@ -23,7 +26,6 @@ const AddedCompanies = styled.div`
 `;
 
 const FoundCompanies = styled.div`
-  border-top: 2px solid ${blackColor};
   margin-top: 8px;
   padding: 8px;
 `;
@@ -61,10 +63,6 @@ const SearchCompanyHeader = styled.div`
   font-size: 30px;
   margin-bottom: 6px;
   text-align: center;
-`;
-
-const SearchButton = styled.button`
-  border: 2px solid ${blackColor};
 `;
 
 const Divider = styled.div`
@@ -108,88 +106,119 @@ class App extends Component {
   }
 
   searchForCompanyHandler = async ({ target: { value } }) => {
-    const url = `https://autocomplete.clearbit.com/v1/companies/suggest?query=${value}`;
+    const searchWithSymbolEndpoint = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${process.env.REACT_APP_ALPHAVANTAGE_API_KEY}`;
+    const { data: { bestMatches: companies } } = await axios.get(searchWithSymbolEndpoint);
 
-    const { data } = await axios.get(url);
-
-    this.setState({
-      foundCompanies: data,
-    });
+    if (companies && companies.length) {
+      this.setState({
+        foundCompanies: companies,
+      });
+    }
   };
 
-  addCompanyHandler = (company) => {
+  addCompanyHandler = async (foundCompany) => {
+    const logoEndpoint = `https://autocomplete.clearbit.com/v1/companies/suggest?query=${foundCompany[nameAttr]}`;
+    const { data: companiesWithLogos } = await axios.get(logoEndpoint);
+
+    const uniqCompanies = companiesWithLogos.reduce(
+      (companies, currentCompany) => (
+        companies.find(comp => comp.name === currentCompany.name)
+          ? companies
+          : companies.concat(currentCompany)
+      ),
+      [],
+    );
+
     this.setState(({ addedCompanies }) => {
-      const isAdded = addedCompanies.find(addedCompany => addedCompany.name === company.name);
+      const uniqCompaniesNames = uniqCompanies.map(({ name }) => name);
+      const addedCompaniesNames = addedCompanies.map(({ name }) => name);
 
-      return isAdded ? {} : { addedCompanies: addedCompanies.concat(company) };
+      const notAddedCompaniesNames = uniqCompaniesNames
+        .filter(x => !addedCompaniesNames.includes(x))
+        .concat(addedCompaniesNames.filter(x => !uniqCompaniesNames.includes(x)));
+
+      return {
+        addedCompanies: addedCompanies.concat(
+          notAddedCompaniesNames.map(
+            notAddedCompanyName => uniqCompanies.find(({ name }) => name === notAddedCompanyName),
+          ),
+        ),
+      };
     });
   };
 
-   removeCompanyHandler = (company) => {
-     const isRemovingConfirmed = window.confirm('Are you sure to remove?');
+  removeCompanyHandler = (company) => {
+    const isRemovingConfirmed = window.confirm('Are you sure to remove?');
 
-     if (isRemovingConfirmed) {
-       this.setState(({ addedCompanies }) => ({
-         addedCompanies: addedCompanies.filter(addedCompany => addedCompany.name !== company.name),
-       }));
-     }
-   };
+    if (isRemovingConfirmed) {
+      this.setState(({ addedCompanies }) => ({
+        addedCompanies: addedCompanies.filter(
+          addedCompany => addedCompany.name !== company.name,
+        ),
+      }));
+    }
+  };
 
-   static renderCompanies(companies, getComponent = () => null) {
-     return (
-       companies.map((company) => {
-         const { name, logo } = company;
+  static renderCompanies(companies, getComponent = () => null) {
+    return (
+      companies.map((company, companyIdx) => {
+        const { [symbolAttr]: symbol } = company;
+        const companyName = company.name || company[nameAttr];
 
-         return (
-           <Company key={`${name}:${logo}`}>
-             <div style={{ width: '40%' }}>
-               { name }
-             </div>
-             <Logo>
-               <img alt={logo} src={logo} />
-             </Logo>
-             { getComponent(company) }
-           </Company>
-         );
-       })
-     );
-   }
+        return (
+          <Company key={`${companyName}:${symbol}:${companyIdx}`}>
+            <div style={{ width: '40%' }}>
+              { companyName }
+            </div>
+            { getComponent(company) }
+          </Company>
+        );
+      })
+    );
+  }
 
-   render() {
-     const { state } = this;
+  render() {
+    const { state } = this;
 
-     return (
-       <Container>
-         <CompanyAdder>
-           <SearchCompanyHeader>
-            SEARCH
-           </SearchCompanyHeader>
-           <SearchInput onChange={this.searchForCompanyHandler} />
-           <SearchButton>search</SearchButton>
-           <FoundCompanies>
-             {
-              App.renderCompanies(
-                state.foundCompanies,
-                company => <Button onClick={() => this.addCompanyHandler(company)}>add</Button>,
-              )
-            }
-           </FoundCompanies>
-         </CompanyAdder>
-         <Divider />
-         <AddedCompanies>
-           <AddedCompaniesHeader>
-            ADDED COMPANIES
-           </AddedCompaniesHeader>
-           {
+    return (
+      <Container>
+        <CompanyAdder>
+          <SearchCompanyHeader>
+          SEARCH
+          </SearchCompanyHeader>
+          <SearchInput onChange={this.searchForCompanyHandler} />
+          <FoundCompanies>
+            {
             App.renderCompanies(
-              state.addedCompanies,
-              company => <Button onClick={() => this.removeCompanyHandler(company)}>X</Button>,
+              state.foundCompanies,
+              company => <Button onClick={() => this.addCompanyHandler(company)}>add</Button>,
             )
           }
-         </AddedCompanies>
-       </Container>
-     );
-   }
+          </FoundCompanies>
+        </CompanyAdder>
+        <Divider />
+        <AddedCompanies>
+          <AddedCompaniesHeader>
+          ADDED COMPANIES
+          </AddedCompaniesHeader>
+          {
+          App.renderCompanies(
+            state.addedCompanies,
+            company => (
+              <React.Fragment>
+                <Logo>
+                  <img alt={company.logo} src={company.logo} />
+                </Logo>
+
+                <Button onClick={() => this.removeCompanyHandler(company)}>X</Button>
+              </React.Fragment>
+            ),
+          )
+        }
+        </AddedCompanies>
+      </Container>
+    );
+  }
 }
 
 export default App;
